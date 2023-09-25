05ea647fb31385bbefc35545baae8ebc51f1a66c
infinex-exchange <admin@infinex.cc>
2023-09-25 23:53:21 +0200

    Refactor 3

--- config.sample.inc.php
+++ config.sample.inc.php
@@ -2,15 +2,19 @@
 
 define('LOG_LEVEL', 0);
 
-define('RMQ_HOST', 'localhost');
-define('RMQ_PORT', 5672);
-define('RMQ_USER', 'authapi');
-define('RMQ_PASS', 'password');
+define('AMQP_HOST', 'localhost');
+define('AMQP_PORT', 5672);
+define('AMQP_USER', 'account.accountd');
+define('AMQP_PASS', 'password');
+define('AMQP_VHOST', '/');
 
 define('DB_HOST', 'localhost');
-define('DB_USER', 'authapi');
+define('DB_USER', 'account.accountd');
 define('DB_PASS', 'password');
-define('DB_NAME', 'infinex_auth');
+define('DB_NAME', 'account.accountd');
+
+define('HTTP_BIND_ADDR', '0.0.0.0');
+define('HTTP_BIND_PORT', 8080);
 
 define('CAPTCHA_SALT', 'AAAaaa');
 
--- src/App.php
+++ src/App.php
@@ -1,63 +1,130 @@
 <?php
 
 require __DIR__.'/MFA.php';
-require __DIR__.'/SignupAPI.php';
-require __DIR__.'/SessionsApiKeysAPI.php';
-require __DIR__.'/PasswordAPI.php';
-require __DIR__.'/EmailAPI.php';
-require __DIR__.'/MFAAPI.php';
+require __DIR__.'/Signup.php';
+require __DIR__.'/Sessions.php';
+require __DIR__.'/Password.php';
+require __DIR__.'/Email.php';
 
-class App extends Infinex\App\Daemon {
+use React\Promise;
+
+class App extends Infinex\App\App {
     private $pdo;
+    private $rest;
     private $mfa;
-    private $api;
+    private $signup;
+    private $sessions;
+    private $password;
+    private $email;
     
     function __construct() {
-        parent::__construct('auth.api-v2');
+        parent::__construct('account.accountd');
         
-        $this -> pdo = new Infinex\Database\PDO($this -> loop, $this -> log);
-        $this -> pdo -> start();
-        
-        $this -> mfa = new MFA($this -> log, $this -> amqp, $this -> pdo);
-        
-        $this -> api = new Infinex\API\API(
+        $this -> pdo = new Infinex\Database\PDO(
+            $this -> loop,
             $this -> log,
-            'api_account',
-            [
-                new SignupAPI(
-                    $this -> log,
-                    $this -> amqp,
-                    $this -> pdo
-                ),
-                new SessionsApiKeysAPI(
-                    $this -> log,
-                    $this -> amqp,
-                    $this -> pdo,
-                    $this -> mfa
-                ),
-                new PasswordAPI(
-                    $this -> log,
-                    $this -> amqp,
-                    $this -> pdo
-                ),
-                new EmailAPI(
-                    $this -> log,
-                    $this -> amqp,
-                    $this -> pdo
-                ),
-                new MFAAPI(
-                    $this -> log,
-                    $this -> amqp,
-                    $this -> pdo,
-                    $this -> mfa
-                )
-            ]
+            DB_HOST,
+            DB_USER,
+            DB_PASS,
+            DB_NAME
         );
         
+        $this -> rest = new Infinex\API\REST(
+            $this -> log,
+            $this -> amqp
+        );
+        
+        $this -> mfa = new MFA(
+            $this -> loop,
+            $this -> log,
+            $this -> amqp,
+            $this -> pdo
+        );
+        
+        $this -> signup = new Signup(
+            $this -> log,
+            $this -> pdo
+        );
+        
+        $this -> sessions = new Sessions(
+            $this -> loop,
+            $this -> log,
+            $this -> pdo,
+            $this -> mfa
+        );
+        
+        $this -> password = new Password(
+            $this -> log,
+            $this -> pdo
+        );
+        
+        $this -> email = new Email(
+            $this -> log,
+            $this -> amqp,
+            $this -> pdo
+        );
+    }
+    
+    public function start() {
         $th = $this;
-        $this -> amqp -> on('connect', function() use($th) {
-            $th -> api -> bind($th -> amqp);
-        });
+        
+        parent::start() -> then(
+            function() use($th) {
+                return $th -> pdo -> start();
+            }
+        ) -> then(
+            function() use($th) {
+                return Promise\all([
+                    $th -> mfa -> start(),
+                    $th -> signup -> start(),
+                    $th -> password -> start(),
+                    $th -> email -> start()
+                ]);
+            }
+        ) -> then(
+            function() use($th) {
+                return $th -> sessions -> start();
+            }
+        ) -> then(
+            function() use($th) {
+                return $th -> rest -> start();
+            }
+        ) -> catch(
+            function($e) {
+                $th -> log -> error('Failed start app: '.((string) $e));
+            }
+        );
+    }
+    
+    public function stop() {
+        $th = $this;
+        
+        $this -> rest -> stop() -> then(
+            function() use($th) {
+                return $th -> sessions -> stop();
+            }
+        ) -> then(
+            function() use($th) {
+                return Promise\all([
+                    $th -> mfa -> stop(),
+                    $th -> signup -> stop(),
+                    $th -> password -> stop(),
+                    $th -> email -> stop()
+                ]);
+            }
+        ) -> then(
+            function() use($th) {
+                return $th -> pdo -> stop();
+            }
+        ) -> then(
+            function() use($th) {
+                $th -> parentStop();
+            }
+        );
+    }
+    
+    private function parentStop() {
+        parent::stop();
     }
 }
 
